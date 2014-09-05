f2bd5318026968f10c72038053ef155f9202bc0d
Andrea Ceccanti <andrea.ceccanti@gmail.com>
2014-09-05 11:04:34 +0200

    Simplify access rule configuration
    By leveraging spring security access control expression we end up with
    having only two rules to manage all current authz needs.

--- src/main/java/org/italiangrid/storm/webdav/spring/web/SecurityConfig.java
+++ src/main/java/org/italiangrid/storm/webdav/spring/web/SecurityConfig.java
@@ -1,5 +1,7 @@
 package org.italiangrid.storm.webdav.spring.web;
 
+import java.util.Collection;
+
 import javax.servlet.ServletContext;
 
 import org.italiangrid.storm.webdav.authz.VOMSAuthenticationFilter;
@@ -21,6 +23,30 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	ServletContext context;
 
+	private String getROVOAccessRule(Collection<String> vos){
+		StringBuilder accessRule = new StringBuilder();
+		
+		boolean first = true;
+		accessRule.append("hasAnyRole(");
+		
+		for (String vo: vos){
+		
+			if (!first){
+				accessRule.append(",");
+			}
+			
+			VOMSVOGrantedAuthority voAuthority = new VOMSVOGrantedAuthority(vo);
+			
+			accessRule.append(String.format("'%s'", voAuthority));
+			
+			first=false;
+		}
+		
+		accessRule.append(")");
+		
+		return accessRule.toString();
+	}
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 
@@ -33,18 +59,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 		StorageAreaInfo sa = (StorageAreaInfo) context
 			.getAttribute(Constants.SA_CONF_KEY);
-
+	
 		if (sa.anonymousReadEnabled() && sa.authenticatedReadEnabled()) {
 
+			String accessRule = String.format("isAnonymous() or isAuthenticated() or %s",
+				getROVOAccessRule(sa.vos()));
+			
 			http.authorizeRequests().requestMatchers(new ReadonlyHTTPMethodMatcher())
-			.anonymous();
-
-			http.authorizeRequests().requestMatchers(new ReadonlyHTTPMethodMatcher())
-				.authenticated();
+				.access(accessRule);
 
 		} else if (sa.authenticatedReadEnabled()) {
+			
+			String accessRule = String.format("isAuthenticated() or %s",
+				getROVOAccessRule(sa.vos()));
+			
 			http.authorizeRequests().requestMatchers(new ReadonlyHTTPMethodMatcher())
-				.authenticated();
+				.access(accessRule);
+			
 		}
 
 		for (String vo : sa.vos()) {
