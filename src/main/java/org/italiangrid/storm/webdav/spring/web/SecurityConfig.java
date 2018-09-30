d3617a0fbe3c1d05686f01b1f0f66e1a407f737b
Andrea Ceccanti <andrea.ceccanti@gmail.com>
2018-09-30 06:54:49 +0200

    initial oauth support skeleton

--- src/main/java/org/italiangrid/storm/webdav/spring/web/SecurityConfig.java
+++ src/main/java/org/italiangrid/storm/webdav/spring/web/SecurityConfig.java
@@ -46,6 +46,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.web.FilterInvocation;
 import org.springframework.security.web.access.expression.WebExpressionVoter;
 import org.springframework.security.web.firewall.RequestRejectedException;
@@ -53,6 +54,7 @@ import org.springframework.web.context.ServletContextAware;
 
 @Configuration
 @EnableWebSecurity
+@EnableAuthorizationServer
 public class SecurityConfig extends WebSecurityConfigurerAdapter implements ServletContextAware {
 
   ServletContext context;
---
+++ src/main/java/org/italiangrid/storm/webdav/spring/web/oauth/OAuthAuthorizationServerConfiguration.java
@@ -0,0 +1,11 @@
+package org.italiangrid.storm.webdav.spring.web.oauth;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+
+@Configuration
+public class OAuthAuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter {
+
+  
+
+}
