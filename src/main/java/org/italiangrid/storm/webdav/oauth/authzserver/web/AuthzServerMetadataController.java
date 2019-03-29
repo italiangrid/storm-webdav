4e67a8e34e849f0519e15d305ac891b26f0f4242
Andrea Ceccanti <andrea.ceccanti@gmail.com>
2019-03-29 17:00:37 +0100

    Improved OIDC/OAuth intialization logic
    And license changes

--- src/main/java/org/italiangrid/storm/webdav/oauth/authzserver/web/AuthzServerMetadataController.java
+++ src/main/java/org/italiangrid/storm/webdav/oauth/authzserver/web/AuthzServerMetadataController.java
@@ -16,7 +16,7 @@
 package org.italiangrid.storm.webdav.oauth.authzserver.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -29,7 +29,7 @@ public class AuthzServerMetadataController {
     this.metadata = md;
   }
 
-  @RequestMapping({".well-known/oauth-authorization-server", ".well-known/openid-configuration"})
+  @GetMapping({".well-known/oauth-authorization-server", ".well-known/openid-configuration"})
   public AuthzServerMetadata getMetadata() {
     return metadata;
   }
