f15f6e4ce2915c8be3725dbb65017f73e1a49578
Federica Agostini <federica.agostini@cnaf.infn.it>
2022-04-13 12:37:27 +0200

    Set timeout to 200 s

--- compose/assets/scripts/wait-and-run-testsuite.sh
+++ compose/assets/scripts/wait-and-run-testsuite.sh
@@ -3,7 +3,7 @@ set -ex
 
 export X509_USER_PROXY=${X509_USER_PROXY:-/tmp/x509up_u$(id -u)}
 DAV_HOST=${DAV_HOST:-storm.example}
-WAIT_TIMEOUT=${WAIT_TIMEOUT:-90}
+WAIT_TIMEOUT=${WAIT_TIMEOUT:-200}
 
 /scripts/init-usercerts.sh
 echo "pass" | voms-proxy-init --cert /tmp/usercerts/test0.p12 -voms test.vo --pwstdin
