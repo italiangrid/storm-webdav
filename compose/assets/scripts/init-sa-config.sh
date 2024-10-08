582a7bfbf55498e1ef61235074b0bd113732895b
Federica Agostini <federica.agostini@cnaf.infn.it>
2024-10-08 17:26:48 +0200

    Remove webdav setup

--- compose/assets/scripts/init-sa-config.sh
+++ compose/assets/scripts/init-sa-config.sh
@@ -2,14 +2,10 @@
 set -ex
 
 SA_CONFIG_DIR=${SA_CONFIG_DIR:-/sa.d}
-VOMAP_CONFIG_DIR=${VOMAP_CONFIG_DIR:-/vo-mapfiles.d}
 CONFIG_DIR=${CONFIG_DIR:-/config}
 
 cp ${SA_CONFIG_DIR}/* /etc/storm/webdav/sa.d
 chown -R storm:storm /etc/storm/webdav/sa.d
-
-cp ${VOMAP_CONFIG_DIR}/* /etc/storm/webdav/vo-mapfiles.d
-chown -R storm:storm /etc/storm/webdav/vo-mapfiles.d
 
 cp ${CONFIG_DIR}/* /etc/storm/webdav/config
 chown -R storm:storm /etc/storm/webdav/config
--- compose/assets/scripts/run.sh
+++ compose/assets/scripts/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -ex
 
+/scripts/setup.sh
 /scripts/unpack-tarball.sh
 /scripts/run-service.sh
