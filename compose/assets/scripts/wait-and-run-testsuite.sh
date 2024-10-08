c78d156804af623e44cdfbe97a2ff85faf7e86b4
Federica Agostini <federica.agostini@cnaf.infn.it>
2024-10-08 15:07:17 +0200

    Remove unused script

--- compose/assets/scripts/wait-and-run-testsuite.sh
+++ compose/assets/scripts/wait-and-run-testsuite.sh
@@ -3,7 +3,6 @@ set -ex
 
 export X509_USER_PROXY=${X509_USER_PROXY:-/tmp/x509up_u$(id -u)}
 DAV_HOST=${DAV_HOST:-storm.test.example}
-WAIT_TIMEOUT=${WAIT_TIMEOUT:-200}
 
 IAM_CLIENT_ID=${IAM_CLIENT_ID:-34d7ff8a-469e-4051-b95a-64b1fa6f7024}
 IAM_TOKEN_ENDPOINT=${IAM_TOKEN_ENDPOINT:-https://wlcg.cloud.cnaf.infn.it/token}
@@ -14,8 +13,6 @@ export IAM_ACCESS_TOKEN=$(curl -d grant_type=client_credentials \
 
 /scripts/init-usercerts.sh
 echo "pass123" | voms-proxy-init --cert /tmp/usercerts/test0.p12 -voms test.vo --pwstdin
-
-/scripts/wait-for-it.sh ${DAV_HOST}:8085 --timeout=${WAIT_TIMEOUT}
 
 cp -r /code/robot .
 
